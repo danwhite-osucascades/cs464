26bf442e96a12fbdf84d75dc73c628bdfc13ed0f
Kyle <canepak@oregonstate.edu>
2026-04-09 21:41:11 -0700

    Updated page.tsx to display a list of all mtg sets based on the json file including a title and description using MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import mtgData from "../../sample_data/mtg.json";
+
+type Item = {
+  name: string;
+  order: number;
+};
+
+type MTGData = {
+  title: string;
+  description: string;
+  items: Item[];
+};
+
+const mtg = mtgData as MTGData;
+
 export default function Home() {
   return (
     <main>
-      <div>Welcome to CS464!</div>
+      <Container maxWidth="md" sx={{ py: 4 }}>
+        <Typography variant="h4" component="h1">
+          {mtg.title}
+        </Typography>
+        <Typography variant="body1" component="p">
+          {mtg.description}
+        </Typography>
+
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <List>
+            {mtg.items.map((item) => (
+              <ListItem key={item.order} divider>
+                <ListItemText
+                  primary={item.name}
+                  secondary={`Order: ${item.order}`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Container>
     </main>
   );
 }
