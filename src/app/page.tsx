338d6171b96f1d888bba0dabe40349bdcd46b07c
Curtis Kauer <kauercurtis@gmail.com>
2026-04-09 21:27:25 -0700

    API route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,6 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-        const response = await fetch('/api/sample_data');
-        const result = await response.json();
-        setData(result);
-        console.log(data);
-     }
-   fetchData();
-   }, []);
-
   return (
     <main>
       <div>Welcome to CS464!</div>
