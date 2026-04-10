c2d91958d5ff41a87a0f2b0e8f5e30cb71de5fba
brookham <bhamilton3264@gmail.com>
2026-04-09 19:49:09 -0700

    kson data mapped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+`use client`
 
 import birdPop from '../../sample_data/bird_pop.json';
 import {
@@ -19,10 +19,34 @@ type Item = {
 };
 
 export default function Home() {
+
+  const items = (birdPop.items as Item[]);
+
   return (
-    <main>
-      <div>Welcome to CS464!</div>
-    </main>
+    <Box sx={{
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <Container maxWidth="sm">
+        <Paper elevation={3} sx={{ padding: 4 }}>
+          <Typography variant="h4" gutterBottom>
+            {birdPop.title}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            {birdPop.description}
+          </Typography>
+          <Stack spacing={2} mt={2}>
+            {items.map((item) => (
+              <Chip key={item.name} label={`${item.name} (Order: ${item.order})`} />
+            ))}
+          </Stack>
+        </Paper>
+      </Container>
+
+    </Box>
   );
 }
 
